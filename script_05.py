a9a2946559e38bb69c6c64c2a356936d57236628
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-11-22 18:45:10 -0300

    Aula 05 Funções recursivas

---
+++ script_05.py
@@ -0,0 +1,31 @@
+"""
+Funções recursivas (recursividade)
+São funções que chamam a si própria.
+
+Exemplo de função recursiva: cálculo do fatorial de um número inteiro.
+5! -> 5 * 4 * 3 * 2 * 1 -> 120
+
+"""
+
+
+def fatorial_nao_recursivo(numero):
+    contador = numero
+    total = numero
+
+    while contador > 1:
+        total *= (contador - 1)
+        contador -= 1
+
+    return total
+
+
+def fatorial(numero):
+    if numero == 1:
+        return numero
+
+    return numero * fatorial(numero - 1)
+
+
+if __name__ == "__main__":
+    print(fatorial_nao_recursivo(5))
+    print(fatorial(5))
